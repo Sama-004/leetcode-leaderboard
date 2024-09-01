e41f05a2b2112aa93d0d9f598b3ad5b5127013cb
sama-004 <70210929+Sama-004@users.noreply.github.com>
2024-09-01 10:19:47 +0530

    added skeleton on rooms page

--- src/app/dashboard/rooms/page.tsx
+++ src/app/dashboard/rooms/page.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+import { Skeleton } from '@/components/ui/skeleton';
 
 // TODO: send metadata logic to server side
 // export const metadata: Metadata = {
@@ -56,7 +57,26 @@ export default function Page() {
   }, [toast]);
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="container mx-auto p-4">
+        <h1 className="text-2xl font-bold mb-4">Your Joined Rooms</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {[...Array(6)].map((_, index) => (
+            <Card key={index} className="bg-black text-white">
+              <CardHeader>
+                <Skeleton className="h-6 w-3/4" />
+              </CardHeader>
+              <CardContent>
+                <Skeleton className="h-4 w-1/2 mb-2" />
+                <Skeleton className="h-4 w-1/3 mb-2" />
+                <Skeleton className="h-4 w-1/4 mb-2" />
+                <Skeleton className="h-8 w-1/3 mt-2" />
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </div>
+    );
   }
 
   return (
@@ -73,7 +93,6 @@ export default function Page() {
               </CardHeader>
               <CardContent>
                 <p>Room Code: {room.code}</p>
-                {/* Change email to lc username*/}
                 {/* Label owner for rooms created by me */}
                 <p>Creator: {room.creator.leetCodeUsername}</p>
                 <p>Participants: {room.participants.length}</p>
